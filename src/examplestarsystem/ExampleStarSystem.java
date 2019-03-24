b77c488224dcf73b724400cba7f6165c36620c33
borboron <48560477+borboron@users.noreply.github.com>
2019-03-24 23:44:43 +0000

    hey there new

--- src/examplestarsystem/ExampleStarSystem.java
+++ src/examplestarsystem/ExampleStarSystem.java
@@ -8,8 +8,11 @@ package examplestarsystem;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -21,95 +24,137 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Priority;
+
 
 /**
  *
  * @author 8753
  */
-public class ExampleStarSystem extends Application {
+public class ExampleStarSystem extends Application{
+        
     StackPane mainMenuStack = new StackPane();
     VBox mainMenuVbox = new VBox();
     VBox settingVbox = new VBox();
+    GridPane selectionpane = new GridPane();
     StackPane settingmenu = new StackPane();
     Group root = new Group();
-    GridPane hello = new GridPane();
+   // GridPane hello = new GridPane();
+    StackPane Selection = new StackPane();
+    Screen screen = Screen.getPrimary();
+    Rectangle2D bounds = screen.getVisualBounds();
+    StackPane loadmenu = new StackPane();
+    VBox save = new VBox();
     
-        public void settingButton() {
-        Image image = new
-        Image(getClass().getResourceAsStream("set8.png"));
-        ImageView isetting = new ImageView(image);
-        
-        isetting.setOnMouseClicked((MouseEvent e)->
-        {
-        System.out.println("Works");
-        root.getChildren().clear();
-        root.getChildren().add(settingmenu);
-        
-        
-        isetting.setY(300);
-        isetting.setX(700);
-        
-        isetting.setFitHeight(30);
-         isetting.setFitWidth(30);
-        
-        
-        });
-        mainMenuStack.getChildren().add(isetting);
- 
+    
+     public static void main(String[] args) {
+        launch(args);
     }
     
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // primaryStage.setFullScreen(true);
+        Stage window = primaryStage;
+        callingsubroutines(); 
+//        window.setX(bounds.getMinX());
+//        window.setY(bounds.getMinY());
+//        window.setWidth(bounds.getWidth());
+//        window.setHeight(bounds.getHeight());
+        
+        root.getChildren().add(mainMenuStack);
+      
+        Scene scene = new Scene(root, 800,580);
+        scene.getStylesheets().add("System.css");
+        primaryStage.setResizable(false);
+     
+        window.setTitle("Solalsystem.exe");
+        window.setScene(scene);
+        window.show();
+    }
+    
+       public void callingsubroutines() {      
+        setupMain();
+        settings();
+        settingButton();
+        Selection();
+        savepage();
+    }
+      
     private void setupMain() {
         Button btn = new Button();
         btn.setText("Create a Simulation");
-        btn.setTranslateY(300);
-        btn.setTranslateX(400);
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Choose as star");
-            }
+        btn.setTranslateY(190);
+        btn.setTranslateX(320);
+        btn.setMaxWidth(220);
+    //    btn.setStyle("-fx-font-weight: bold");
+        btn.setStyle("-fx-border-radius: 40px");
+        btn.setStyle("-fx-padding: 10px");
+        btn.setStyle("-fx-font-size: 20px");
+       
+       
+        btn.setOnAction((ActionEvent event)-> {
+            root.getChildren().clear();
+            root.getChildren().add(Selection);
         });
         
-        Button btn1 = new Button();
-        btn1.setTranslateY(10);
-        btn1.setTranslateX(700);
-        btn1.setText("Go to the settings page");
-       
+//        Button btn1 = new Button();
+//        btn1.setText("Settings");
+//        btn1.setTranslateY(-70);
+//        btn1.setTranslateX(730);
+//        btn1.setStyle("-fx-padding: 5px");
+//        btn1.setOnAction((ActionEvent event) -> {
+//           root.getChildren().clear();
+//           root.getChildren().add(settingmenu);
+//        });
         
-       btn1.setOnAction((ActionEvent event) -> {
+        Button btn2 = new Button();
+        btn2.setText("Previous Simulations");
+        btn2.setTranslateY(240);
+        btn2.setTranslateX(325);
+        btn2.setStyle("-fx-padding: 5px");
+        btn2.setStyle("-fx-font-weight: bold");
+        btn2.setStyle("-fx-background-radius:50");
+        btn2.setStyle("-fx-font-size: 20px");
+        btn2.setOnAction((ActionEvent event)-> {
            root.getChildren().clear();
-           root.getChildren().add(settingmenu);
+           root.getChildren().add(loadmenu);
+        
         });
-    
           
-       
-                
         Label Title = new Label();
         Title.setText("Solar System Simulator");
-        Title.setStyle("-fx-font-size:20px;");
+        Title.setStyle("-fx-font-size:30px;");
         Title.setTextFill(Color.web("#FFFFFF"));
         Title.setTranslateY(10);
-        Title.setTranslateX(350);
+        Title.setTranslateX(265);
        
                
         
         
-        //create a background image
-         Image imgBack = new
-            Image(getClass().getResourceAsStream("32.png"));
+       //create a background image
+        Image imgBack = new       
+        Image(getClass().getResourceAsStream("32.png"));
+         
         ImageView ivBack = new ImageView(imgBack);
+        //ivBack.setFitWidth(1300);
+//        ivBack.setFitHeight(1000);
+        ivBack.setPreserveRatio(true);
+        
         mainMenuStack.getChildren().add(ivBack);
         
         mainMenuStack.getChildren().add(mainMenuVbox);  
-        mainMenuVbox.getChildren().addAll(Title, btn, btn1);
+        mainMenuVbox.getChildren().addAll(Title, btn, btn2);
         
 //        //add celestial objects               
 //         Planet p = new Planet(500, 40, 9.8, "Earth2", Color.BLUE, 300, 300);
 //         Planet s = new Planet(500, 40, 9.8, "Earth3", Color.ORANGE, 90, 50);
-//          Planet r = new Planet(500, 40, 9.8, "Earth4", Color.PURPLE, 400, 200);
+//          Planet r = new Planet(500, 40, 9.8, "Earth4", Color.PURPLE   , 400, 200);
 //           Planet n = new Planet(500, 40, 9.8, "Earth5", Color.RED, 50, 50);
 //            Planet i = new Planet(500, 40, 9.8, "Earth6", Color.GOLD, 500, 150);
 //        Star q = new Star(1000, 100, 9.8, "Sun" , Color.YELLOW, 400,300);
+//        
 //        
 //       
 //       mainMenuStack.getChildren().add(p.getCircle());
@@ -118,8 +163,36 @@ public class ExampleStarSystem extends Application {
 //       mainMenuStack.getChildren().add(r.getCircle());
 //       mainMenuStack.getChildren().add(i.getCircle());
 //       mainMenuStack.getChildren().add(n.getCircle());
-//        
-       // root.getChildren().add(mainMenuStack);
+        
+        
+       
+    }
+    
+    
+    private void savepage() {
+        Button b2 = new Button();
+        b2.setText("Main menu");
+        b2.setOnAction((ActionEvent event)-> {
+            root.getChildren().clear();
+            root.getChildren().add(mainMenuStack);
+        });
+        
+        Image imgBack = new Image(getClass().getResourceAsStream("32.png"));
+        ImageView ivBack = new ImageView(imgBack);
+        loadmenu.getChildren().add(ivBack);
+        
+                Label header = new Label();
+        header.setText("Previous Saves");
+        header.setStyle("-fx-font-size:30px;");
+        header.setTextFill(Color.web("#FFFFFF"));
+        header.setTranslateY(10);
+        header.setTranslateX(310);
+        
+        loadmenu.getChildren().add(save);  
+        save.getChildren().addAll(header);
+        
+        
+        
     }
     
     private void settings() {
@@ -131,22 +204,14 @@ b1.setOnAction((ActionEvent event) -> {
            root.getChildren().add(mainMenuStack);
         });
 
-
-
-
-
-  Label Title = new Label();
-        Title.setText("Setting");
-        Title.setStyle("-fx-font-size:20px;");
+        Label Title = new Label();
+        Title.setText("Settings");
+        Title.setStyle("-fx-font-size:30px;");
         Title.setTextFill(Color.web("#FFFFFF"));
         Title.setTranslateY(10);
         Title.setTranslateX(350);
 
-
-
-
- Image imgBack = new
-            Image(getClass().getResourceAsStream("32.png"));
+        Image imgBack = new Image(getClass().getResourceAsStream("32.png"));
         ImageView ivBack = new ImageView(imgBack);
         
         
@@ -157,33 +222,57 @@ b1.setOnAction((ActionEvent event) -> {
     }
     
     
-    public void callingsubroutines() {
+            public void settingButton() {
+        // Createing an image    
+        Image image = new Image(getClass().getResourceAsStream("settingicon.png"));
+        //Setting the isetting
+        ImageView isetting = new ImageView(image);
+        //Setting the position of the image
+        isetting.setTranslateY(-260);
+        isetting.setTranslateX(320);
         
-        setupMain();
-        settings();
-        settingButton();
-       
-        
+        isetting.setFitHeight(50);
+        isetting.setFitWidth(50);
+        //Creating an event, activated by clicking the image
+        isetting.setOnMouseClicked((MouseEvent e)->{
+        System.out.println("Works");
+        root.getChildren().clear();
+        root.getChildren().add(settingmenu);   
+        });
+        mainMenuStack.getChildren().add(isetting);
+ 
     }
+        private void Selection() {
+            Label title = new Label();
+            title.setText("Select Objects");
+            title.setStyle("-fx-font-size:30px;");
+            title.setTextFill(Color.web("#FFFFFF"));
+            title.setTranslateY(10);
+            title.setTranslateX(310);
+            
+            Image imgBack = new Image(getClass().getResourceAsStream("32.png"));
+            ImageView ivBack = new ImageView(imgBack);
+        
+           Label nstars = new Label("Number of Stars");
+           Slider star = new Slider(0,2,1);
+           star.setShowTickLabels(true);
+           star.setShowTickMarks(true);
+           star.setMajorTickUnit(2);
+           star.setMinorTickCount(5);
+           star.setBlockIncrement(10);
+           star.setTranslateX(50);
+           star.setTranslateY(100);
+           star.setMaxWidth(1000);
+           GridPane.setHgrow(star, Priority.NEVER);
+           Slider planet = new Slider(0,10,1);
+           
+        
+           Selection.getChildren().add(ivBack);
+           Selection.getChildren().add(selectionpane);
+           selectionpane.getChildren().addAll(title, star, nstars, planet);
+            
+        }    
     
-    @Override
-    public void start(Stage primaryStage) {
-        callingsubroutines(); 
 
-        root.getChildren().add(mainMenuStack);
-        Scene scene = new Scene(root, 800,600);
-        //scene.getStylesheets().add("path/Style.css");
-               
-        primaryStage.setTitle("Solalsystem.exe");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
     
 }
--- src/examplestarsystem/Slider.java
+++ src/examplestarsystem/Slider.java
@@ -7,8 +7,55 @@ package examplestarsystem;
 
 /**
  *
- * @author 8753
+ * @author cyber
  */
 public class Slider {
+    private int max;
+    private int min;
+    private int mid;
+    
+    Slider(int min, int max, int mid) {
+        this.min = min;
+        this.max = max;
+        this.mid = mid;
+        
+    }
+        public double getMax() {
+        return max;
+    }
+
+    /**
+     * @param max the mass to set
+     */
+    public void setMax(double mass) {
+        this.max = max;
+    }
+    
+        public double getMin() {
+        return min;
+    }
+
+    /**
+     * @param min the mass to set
+     */
+    public void setMin(double mass) {
+        this.min = min;
+    }
+    
+        public double getMid() {
+        return mid;
+    }
+
+    /**
+     * @param mid the mass to set
+     */
+    public void setMid(double mass) {
+        this.mid = mid;
+    }
+
+
+
+    
     
 }
+
